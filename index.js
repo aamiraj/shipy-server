a97c554794a0bc9c0a3ca83c5b9024803c4cfe1f
AAMiraj <miraj2465@gmail.com>
2022-11-09 01:19:18 +0600

    services data cen be read

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = 5000 || process.env.PORT;
+const cors = require("cors");
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World! Server is running...");
+app.use(cors());
+//const data = require("./data.json");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ghnljed.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
+
+function run() {
+  try {
+    const database = client.db("shipy-services");
+    const servicesCollection = database.collection("services");
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const objectId = ObjectId(id);
+      console.log(objectId);
+      const query = { _id: objectId };
+      const service = await servicesCollection.findOne(query);
+      res.send(service);
+    });
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run();
 
 app.listen(port, () => {
   console.log(`Shipy app listening on port ${port}`);
