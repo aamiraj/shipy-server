4729f968254dd074ae0a7fb5616b6a6c39bfc7ac
AAMiraj <miraj2465@gmail.com>
2022-11-10 21:45:43 +0600

    added jwt authentication for my review

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = 5000 || process.env.PORT;
 const cors = require("cors");
 require("dotenv").config();
 
+//console.log(require("crypto").randomBytes(64).toString("hex"));
+
 app.use(cors());
 app.use(express.json());
 //const data = require("./data.json");
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ghnljed.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -22,11 +42,27 @@ function sortByDate(a, b) {
   return new Date(b.date) - new Date(a.date);
 }
 
-function run() {
+async function run() {
   try {
     const database = client.db("shipy-services");
     const servicesCollection = database.collection("services");
     const reviewsCollection = database.collection("reviews");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ token });
+    });
+
+    app.post("/add-service", async (req, res) => {
+      const doc = req.body;
+      //console.log(doc);
+      const result = await servicesCollection.insertOne(doc);
+      console.log(result);
+      res.send(result);
+    });
 
     app.post("/review", async (req, res) => {
       const doc = req.body;
@@ -35,7 +71,15 @@ function run() {
       res.send(result);
     });
 
-    app.get("/my-reviews", async (req, res) => {
+    app.delete("/my-reviews/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const objectId = ObjectId(id);
+      const query = { _id: objectId };
+      const result = await reviewsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/my-reviews", verifyJWT, async (req, res) => {
       const query = req.query;
       const cursor = reviewsCollection.find(query);
       const result = await cursor.toArray();
